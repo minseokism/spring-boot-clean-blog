595accdda21e236681f2f5719d0021411eee4ec0
Minseok Lee <lagender@gmail.com>
2018-07-11 17:44:05 +0900

    [spring-boot] 블로그를 만들자. (2) JPA - test code 수정2

--- src/test/java/com/minseokism/post/PostControllerTest.java
+++ src/test/java/com/minseokism/post/PostControllerTest.java
@@ -39,7 +39,7 @@ public class PostControllerTest {
                 .andReturn();
 
         Post post = (Post) mvcResult.getModelAndView().getModel().get("post");
-        assertThat(post.getTitle()).isEqualTo("자바");
+        assertThat(post.getTitle()).isEqualTo("제목");
         assertThat(post.getContent()).isEqualTo("컨텐츠");
         assertThat(post.getCode()).isEqualTo("마크다운");
         assertThat(post.getStatus()).isEqualTo(PostStatus.Y);
@@ -63,7 +63,7 @@ public class PostControllerTest {
                 .andReturn();
 
         PostDto postDto = (PostDto) mvcResult.getModelAndView().getModel().get("editPost");
-        assertThat(postDto.getTitle()).isEqualTo("자바");
+        assertThat(postDto.getTitle()).isEqualTo("제목");
         assertThat(postDto.getContent()).isEqualTo("컨텐츠");
         assertThat(postDto.getCode()).isEqualTo("마크다운");
     }

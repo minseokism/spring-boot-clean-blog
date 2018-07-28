87436f681cf0752ce9043a97c3ec65bde374c307
Minseok Lee <lagender@gmail.com>
2018-07-28 11:13:56 +0900

    [spring-boot] 블로그를 만들자. (4) thymeleaf - 설정 변경

--- src/main/java/com/minseokism/Application.java
+++ src/main/java/com/minseokism/Application.java
@@ -3,7 +3,9 @@ package com.minseokism;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 @EntityScan(basePackageClasses = {Application.class, Jsr310JpaConverters.class})
@@ -13,4 +15,8 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public SpringDataDialect springDataDialect() {
+        return new SpringDataDialect();
+    }
 }
--- src/main/java/com/minseokism/index/IndexController.java
+++ src/main/java/com/minseokism/index/IndexController.java
@@ -1,6 +1,8 @@
 package com.minseokism.index;
 
+import com.minseokism.post.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class IndexController {
 
+    private final PostRepository postRepository;
+
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("message", "hello world");
+    public String home(Model model, Pageable pageable) {
+        model.addAttribute("posts", postRepository.findAll(pageable));
         return "index";
     }
 }
